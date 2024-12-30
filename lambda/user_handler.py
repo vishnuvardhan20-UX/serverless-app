4db2d76dd7dc93cc1bee5fab76b1de54b5e31982
Yeluri Venkata Vishnu Vardhan <vishnuvardhanyeluri3@gmail.com>
2024-12-30 20:03:31 +0530

    Update user_handler.py

--- lambda/user_handler.py
+++ lambda/user_handler.py
@@ -1,8 +1,15 @@
 import json
 
 def lambda_handler(event, context):
-    name = event.get('name', 'Guest')
+    """
+    AWS Lambda function handler.
+    This function returns a simple response for demonstration purposes.
+    """
     return {
-        'statusCode': 200,
-        'body': json.dumps(f'Hello, {name}!')
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Hello, world! This is a serverless app!",
+            "input": event
+        })
     }
+
